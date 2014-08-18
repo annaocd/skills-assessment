b413c5e13d1f9a2d1b944908c88a99cf09ea600d
anna yovandich <anna.yovandich@ff0000.com>
2014-08-18 12:34:39 -0700

    adding modal hide/show (WIP)

--- static/js/apps/application/ApplicationController.js
+++ static/js/apps/application/ApplicationController.js
@@ -1,1 +1,18 @@
-App.ApplicationController = Ember.ObjectController.extend();
+App.ApplicationController = Ember.ObjectController.extend({
+	isModalShowing: false,
+	isModalActive: false,
+
+	actions: {
+		showModal : function () {
+			this.set('isModalActive', true);
+			Ember.run.next(this, 'set', 'isModalShowing', true);
+		},
+		closeModal : function () {
+			this.set('isModalShowing', false);
+
+			Ember.run.later(this, function () {
+				this.set('isModalActive', false);
+			}, 300);
+		},
+	}
+});
--- static/js/apps/list/ListController.js
+++ static/js/apps/list/ListController.js
@@ -1,1 +1,9 @@
-App.ListController = Ember.ObjectController.extend();
+App.ListController = Ember.ObjectController.extend({
+	needs: ['application'],
+
+	actions: {
+		showModal : function () {
+			this.controllerFor('application').send('showModal');
+		}
+	}
+});
